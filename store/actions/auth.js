702d2398af81bb09ab2300a89eaff0d5e2475128
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-08-06 21:53:02 -0400

    login + signup fixed

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -22,7 +22,7 @@ export const authenticate = (userId, token, expiryTime) => {
 export const signup = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAwgME943HeTkyX9QJak260PNbYZfQI58Q',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyArbUgQS-KwCKXnbLWnjoER3UDPyPW5uMA',
       {
         method: 'POST',
         headers: {
@@ -65,7 +65,7 @@ export const signup = (email, password) => {
 export const login = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAwgME943HeTkyX9QJak260PNbYZfQI58Q',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyArbUgQS-KwCKXnbLWnjoER3UDPyPW5uMA',
       {
         method: 'POST',
         headers: {
